b3ddb3707d7e73fe3d4034f9d5feb741a8ea0dd1
Nicolas Degardin <degardin.n@gmail.com>
2017-11-06 16:00:25 -0500

    Decorators that build the expected schema (only description and option are handled)

--- src/serafin/pipeline/Decorators.ts
+++ src/serafin/pipeline/Decorators.ts
@@ -7,39 +7,22 @@ import { JSONSchema4 } from "json-schema"
  * @param schema JSONSchema definition. Can be an object or a function returning an object
  * @param required true or false
  */
-export function option(property: string, schema: Object | (() => Object), required: boolean = true) {
-    return parameterDecorator('option', property, schema, required);
-}
-
-export function queryProperty(property: string, schema: Object | (() => Object), required: boolean = true) {
-    return parameterDecorator('query', property, schema, required);
-}
-
-export function resourceProperty(property: string, schema: Object | (() => Object), required: boolean = true) {
-    return parameterDecorator('resources', property, schema, required);
-}
-
-export function resourceValue(property: string, schema: Object | (() => Object), required: boolean = true) {
-    return parameterDecorator('values', property, schema, required);
-}
-
-function parameterDecorator(parameterName:string, property: string, schema: Object | (() => Object), required: boolean = true) {
+export function option(option: string, schema: Object | (() => Object), required: boolean = true, description: string = null) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         if (!descriptor.value['properties']) {
             descriptor.value['properties'] = {
-                type: 'object',
-                properties: {
-
+                options: {
+                    type: 'object',
+                    properties: {},
+                    required: []
                 }
             };
         }
 
-      //  descriptor.value['properties'].properties[parameterName].properties[property] = (typeof schema == 'function') ? schema() : schema;
+        descriptor.value['properties'].options.properties[option] = (typeof schema == 'function') ? schema() : schema;
         if (required) {
-           // descriptor.value['properties'].properties[parameterName].required.push(property);
+           descriptor.value['properties'].options.required.push(option);
         }
-
-        console.log();
     }
 }
 
@@ -50,11 +33,11 @@ function parameterDecorator(parameterName:string, property: string, schema: Obje
  */
 export function description(text: string) {
     return function (targetOrCtor: any, propertyKey?: string, descriptor?: PropertyDescriptor) {
-    /*    if (typeof (descriptor) === 'undefined') {
+        if (typeof (descriptor) === 'undefined') {
             targetOrCtor['description'] = text;
         } else {
             descriptor.value.description = text;
-        }*/
+        }
     };
 }
 
