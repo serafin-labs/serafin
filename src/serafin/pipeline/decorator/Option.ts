9f449c81e3d5ee121a19d64b4ef1ad6b72396ae3
Sébastien De Saint Florent <sebastien.dsf@gmail.com>
2017-11-14 12:41:01 -0500

    Add validation to option decorator

--- src/serafin/pipeline/decorator/Option.ts
+++ src/serafin/pipeline/decorator/Option.ts
@@ -1,4 +1,7 @@
+import { type } from 'os';
 import { addPipelineOptionMetadata } from '../Abstract'
+import * as Ajv from 'ajv'
+import { PipelineAbstract } from '../Abstract'
 
 /**
  * Class or method decorator used to declare an action option, along with its JSONSchema definition.
@@ -7,8 +10,9 @@ import { addPipelineOptionMetadata } from '../Abstract'
  * @param schema JSONSchema definition. Can be an object or a function returning an object
  * @param required true or false
  * @param description Description of the option
+ * @param validation Flag indicating if this option should be validated automatically. Default value : 'true'
  */
-export function option(option: string, schema: Object | (() => Object), required: boolean = true, description: string = null) {
+export function option(option: string, schema: Object | (() => Object), required: boolean = true, description: string = null, validation = true) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         // extract schemaObject
         let schemaObject
@@ -22,6 +26,40 @@ export function option(option: string, schema: Object | (() => Object), required
         addPipelineOptionMetadata(target, propertyKey, option, schemaObject, description, required)
 
         // add validation code to the method
-        // TODO add option validation here. @validate only validates schemas related to the model. When an option is passed it always needs to be validated
+        if (validation && typeof descriptor.value == 'function' && PipelineAbstract.getCRUDMethods().find((key) => propertyKey == key)) {
+            let func: Function = descriptor.value;
+
+            // validate function to check this option alone
+            var ajv = new Ajv();
+            ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
+            let validate = ajv.compile({
+                type: 'object',
+                properties: {
+                    [option]: schemaObject
+                },
+                required: required ? [option] : []
+            });
+            // index of the option parameter based on the method name
+            var optPosition = propertyKey === "create" || propertyKey === "read" || propertyKey === "delete" ? 1 : 2;
+
+            // wrap the method to add validation
+            descriptor.value = function (...params) {
+                try {
+                    // get options from params
+                    let options = params.length > optPosition ? params[optPosition] : null;
+                    // run validation
+                    let valid = validate(options || {});
+                    if (!valid) {
+                        throw new Error(ajv.errorsText(validate.errors))
+                    }
+                    // call the real implementation
+                    return func.apply(this, params);
+                } catch (e) {
+                    let callError = new Error("Validation error in " + Object.getPrototypeOf(this).constructor.name + "." + propertyKey + " : " + e);
+                    console.log("Validation error in " + Object.getPrototypeOf(this).constructor.name + "." + propertyKey + " : " + e);
+                    return Promise.reject(e);
+                }
+            };
+        }
     }
 }
