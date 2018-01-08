5c134beba5062ed68cfd95588d9f3f3d005457f4
Nicolas Degardin <degardin.n@gmail.com>
2018-01-08 18:01:36 -0500

    Warehouse sample

--- src/example/warehouse/model/Category.ts
+++ src/example/warehouse/model/Category.ts
@@ -3,6 +3,5 @@ import { SchemaBuilder } from "@serafin/schema-builder"
 export var categorySchemaBuilder = SchemaBuilder.emptySchema()
   .addString("id", { description: "Category identifier" })
   .addString("name", { description: "Category name" })
-  .addOptionalStringArray("itemIds", { description: "Category items" })
   .addOptionalString("parentCategory", { description: "Parent category identifier" })
   .flatType();
--- src/example/warehouse/model/Item.ts
+++ src/example/warehouse/model/Item.ts
@@ -3,4 +3,6 @@ import { SchemaBuilder } from "@serafin/schema-builder"
 export var itemSchemaBuilder = SchemaBuilder.emptySchema()
   .addString("id", { description: "Item identifier" })
   .addString("name", { description: "Item name" })
+  .addNumber("price", { description: "Item price" })
+  .addString("categoryId", { description: "Item category identifier" })
   .flatType();
